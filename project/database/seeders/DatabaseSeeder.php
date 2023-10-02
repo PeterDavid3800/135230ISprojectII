94dde8facb4eee2bb93c748cf94ffbc4777d6e3a
PeterDavid3800 <davidpeter487@gmail.com>
2023-10-02 11:10:29 +0300

    Commit

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +15,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(5)->create();
+
+         Listing:: factory(5)->create();
+         //Listing::create([
+           //'title' => 'Laravel Senior Developer',
+           //'tags' => 'Laravel, Javascript',
+           //'company' => 'Acme Corp',
+           //'location' => 'Boston, MA',
+           //'email' => 'email@email.com',
+           //'website' => 'https://www.acme.com',
+           //'description' => 'Blah blah blah blah blah
+           //blah'
+
+         //]);
+         //Listing::create([
+           //'title' => 'Full Stack Engineer',
+           //'tags' => 'Laravel,backend, API',
+           //'company' => 'Stark Industries',
+           //'location' => 'NewYork, NY',
+           //'email' => 'email2@email.com',
+           //'website' => 'https://www.starkindustries.com',
+           //'description' => 'Blah blah blah blah blah
+           //blah'
+         //]);
 
         // \App\Models\User::factory()->create([
-         //    'name' => 'Test User',
+        //    'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
     }
